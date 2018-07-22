71355fa40cfda8c071609150864f051e932112c9
wonseok <wonseok0403@naver.com>
2018-07-22 18:12:33 +0900

    first commit

---
+++ NodeConsole2/app.js
@@ -0,0 +1,10 @@
+var io = require('socket.io').listen(52723);
+
+io.on("connection", function (socket) {
+    console.log("A user connected");
+
+    socket.on("sMsg", function (data) {
+        console.log(data);
+        io.sockets.emit("rMsg", data);
+    });
+});
